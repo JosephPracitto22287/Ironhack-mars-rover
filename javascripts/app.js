84b508d2e2e7830b346ef4c57bfb2a4b1b244620
Joseph Pracitto <jpracitto22287@columbushs.com>
2020-12-16 13:20:50 -0500

    Mars Rover

--- javascripts/app.js
+++ javascripts/app.js
@@ -13,3 +13,73 @@ function turnRight(rover){
 function moveForward(rover){
   console.log("moveForward was called")
 }
+
+// ITERATION 1
+let rover ={
+  direction: "N"
+}
+
+//ITERATION 2
+function turnLeft(rover){
+  if (rover.direction == "N") {
+    rover.direction = "W";
+} else if (rover.direction ==="W") {
+  rover.direction = "S";
+} else if (rover.direction ==="S") {
+rover.direction = "E";
+} else if (rover.direction === "E") {
+rover.direction = "N"
+}
+}
+
+function turnRight(rover){
+  if (rover.direction == "S") {
+    rover.direction = "E";
+} else if (rover.direction ==="E") {
+  rover.direction = "N";
+} else if (rover.direction ==="N") {
+rover.direction = "W";
+} else if (rover.direction === "W") {
+rover.direction = "S"
+}
+//iteration 3
+console.log("TESTING")
+function moveForwarad(rover) {
+if(rover.direction === "N") {
+  rover.y--;
+  console.log(`X: ${rover.x}, ${rover.y}`)
+} else if (rover.direction === "S") {
+  rover.y++;
+  console.log(`X: ${rover.x}, ${rover.y}`)
+} else if (rover.direction === "E") {
+  rover.x++;
+  console.log(`X: ${rover.x}, ${rover.y}`)
+} else if(rover.direction == "W") {
+  rover.x--;
+  console.log(`X: ${rover.x}, ${rover.y}`)
+}
+}
+
+//ITERATION 4
+function commands(steps) {
+
+for(let i = 0; i < steps.length; i++) {
+  if(steps.charAt(i) === "l") {
+    turnLeft (rover);
+  } else if (steps.charAt(i) === "r") {
+  turnRight(rover);
+} else if else if (steps.charAt(i) === "f") {
+  moveForward(rover);
+}
+
+}
+
+//ITERATION 5
+
+rover.travalLog.push(steps.charAt(i));
+
+}
+
+console.log("Travel Log: " + rover.travelLog);
+
+}
